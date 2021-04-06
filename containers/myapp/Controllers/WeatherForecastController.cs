4579e5e05119e28f157ef3b9ce7b680649263dc4
Jose Maria Hernandez Hernandez <jose.hernandez@druni.es>
2021-04-06 21:17:12 +0200

    Creacion de aplicaciones

---
+++ containers/myapp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace myapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SaludosController : ControllerBase
+    {        
+        private readonly ILogger<SaludosController> _logger;
+
+        public SaludosController(ILogger<SaludosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hola mundo v2");
+        }
+    }
+}
