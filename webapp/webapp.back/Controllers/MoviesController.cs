ae967f8f59c14d28d047020b58a34ff6036ab091
Jose Maria Hernandez Hernandez <jose.hernandez@druni.es>
2021-05-10 17:59:06 +0200

    Correccion de falo de compilacion

--- webapp/webapp.back/Controllers/MoviesController.cs
+++ webapp/webapp.back/Controllers/MoviesController.cs
@@ -28,7 +28,7 @@ namespace webapp.back.Controllers
                 new Movie { Genre = "Miedito", ID = 1, Price = 2.10m, ReleaseDate = DateTime.Now, Title = "The Ring" },
                 new Movie { Genre = "Miedito extremo", ID = 1, Price = 2.10m, ReleaseDate = DateTime.Now, Title = "The Ring 2" },
                 new Movie { Genre = "Miedito extremo", ID = 1, Price = 2.10m, ReleaseDate = DateTime.Now, Title = "It" },
-                new Movie { Genre = "Risa", ID = 2, Price = 2.10m, ReleaseDate = DateTime.Now, Title = "Top Secret" }
+                new Movie { Genre = "Risa", ID = 2, Price = 2.10m, ReleaseDate = DateTime.Now, Title = "Top Secret" },
                 new Movie { Genre = "Risa", ID = 2, Price = 2.10m, ReleaseDate = DateTime.Now, Title = "Regreso al futuro" }
             };
             return Ok(peliculas);
