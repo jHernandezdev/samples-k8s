61ace1ce6316c0ccf82ba33cdf988a76cefb5074
Jose Maria Hernandez Hernandez <jose.hernandez@druni.es>
2021-06-02 15:10:48 +0200

    Cambios en el front

--- webapp/webapp.front/Pages/Movies/Index.cshtml.cs
+++ webapp/webapp.front/Pages/Movies/Index.cshtml.cs
@@ -4,15 +4,19 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using webapp.front.Models;
+using Microsoft.Extensions.Logging;
 
 namespace webapp.front.Pages.Movies
 {
     public class MoviesModel : PageModel
     {
         private readonly IHttpClientFactory _clientFactory;
-        public MoviesModel(IHttpClientFactory clientFactory)
+        private readonly ILogger<MoviesModel> _logger;
+
+        public MoviesModel(IHttpClientFactory clientFactory, ILogger<MoviesModel> logger)
         {
             _clientFactory = clientFactory;
+            _logger = logger;
         }
 
         public List<Movie> Peliculas;
@@ -22,6 +26,7 @@ namespace webapp.front.Pages.Movies
             Peliculas = new List<Movie>();
             var client = _clientFactory.CreateClient("webapp.back");
            
+           _logger.LogInformation(client.BaseAddress.ToString());            
             var resultado = await client.GetAsync("/Movies");
             if (resultado.IsSuccessStatusCode)
             {
